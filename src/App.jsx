cdff433a249e81ebe86606339f934c036afe2160
hunii ali <huniiali1122@gmail.com>
2026-03-31 23:48:31 +0500

    Desturueint completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 
 const App = () => {
-
   return (
-    <div className='h-screen w-full bg-black text-white flex items-center justify-center'>
-      <h1>hey hunain how are u </h1>
-    </div>
+    <div className='h-screen w-full bg-black text-white flex items-center justify-center'>App</div>
   )
 }
 
